223aa4adc7ad653bc85f47b3f291f93874490f60
Joel Hernandez <involvmnt@gmail.com>
2017-06-14 21:32:30 +0200

    refactor(wp-config): remove force ssl

--- web/wp-config.php
+++ web/wp-config.php
@@ -56,7 +56,7 @@ $WP_NONCE_SALT = getenv("WP_NONCE_SALT");
 
 $WP_DEBUG = getenv("WP_DEBUG") === "true";
 
-define('FORCE_SSL_ADMIN', true);
+//define('FORCE_SSL_ADMIN', true);
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
