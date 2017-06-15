79eff3ea20a45e76f42043dcf248654c27d17dcf
Joel Hernandez <involvmnt@gmail.com>
2017-06-15 23:06:17 +0200

    feat(wp-config): dont load dotenv on production

--- web/wp-config.php
+++ web/wp-config.php
@@ -22,8 +22,14 @@ $root_dir = __DIR__ . '/../';
 
 require($root_dir . 'vendor/autoload.php');
 
-$dotenv = new Dotenv\Dotenv($root_dir);
-$dotenv->load();
+
+$ENVIRONMENT = getenv('ENVIRONMENT');
+
+if($ENVIRONMENT !== 'PRODUCTION'){
+	$dotenv = new Dotenv\Dotenv($root_dir);
+	$dotenv->load();
+}
+
 
 $DB_URL = getenv("DATABASE_URL");
 
